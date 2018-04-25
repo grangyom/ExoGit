f3a749a188769c9d8b8bd925543097cf2b4110e2
Mustapha <Mustapha.boulouh@icloud.com>
2018-04-25 11:45:48 +0200

    Méthode soustraction

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,19 @@ public class Main {
 	public static void main(String[] args) {
 		int result;
 		result = calculSomme(4,2);
-		System.out.println(result);		
+		result = Soutract(4,2);
+		System.out.println(result);
 	}
 
 	public static int calculSomme(int a, int b) {
 		int somme;
 		somme = a + b;
+		return somme;
+	}
+
+	public static int Soutract(int a, int b) {
+		int somme;
+		somme = a - b;
 		return somme;
 	}
 }
